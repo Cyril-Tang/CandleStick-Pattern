565676f80f99c4a7367992a707a8797593c1a2fe
Cyril <yifut@student.unimelb.edu.au>
2020-01-02 01:08:03 +1100

    data

--- data_extration.py
+++ data_extration.py
@@ -1,16 +1,50 @@
-import baostock as bs
+import tushare as ts
 import pandas as pd
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
-# login to the baostock system
-lg = bs.login()
-print(lg.error_msg ,': ', lg.error_code)
+start_date = '2000-01-01'
+end_date = '2015-12-31'
 
-#### 获取沪深A股历史K线数据 ####
-# 详细指标参数，参见“历史行情指标参数”章节；“分钟线”参数与“日线”参数不同。
-# 分钟线指标：date,time,code,open,high,low,close,volume,amount,adjustflag
-rs = bs.query_history_k_data_plus("sh.600000",
-    "date,code,open,high,low,close,preclose,volume,amount,adjustflag,turn,tradestatus,pctChg,isST",
-    start_date='2017-07-01', end_date='2017-12-31',
-    frequency="d", adjustflag="3")
-print('query_history_k_data_plus respond error_code:'+rs.error_code)
-print('query_history_k_data_plus respond  error_msg:'+rs.error_msg) 
+pro = ts.pro_api()
+
+# Obtain  all stocks available
+data = pro.query('stock_basic', exchange='', list_status='L', fields='ts_code,symbol,name,area,industry,list_date')
+data = data.dropna()
+data.drop(data[data.symbol.astype('int') > 604000].index, inplace=True)
+
+stock_list = [i for i in data.ts_code]
+
+data.industry = data.industry.astype('category')
+
+
+from multiprocessing.dummy import Pool as ThreadPool
+
+def process(stock):
+    df = ts.pro_bar(ts_code=stock, adj='qfq', start_date=start_date , end_date=end_date)
+    df.sort_index(axis=0, ascending = False, inplace = True)
+    df.reset_index(drop=True, inplace=True)
+    df = df.drop(['pre_close', 'change', 'amount'], axis=1)
+    df.to_csv('Data/'+stock.split('.')[0]+'.csv')
+
+
+
+def main():
+    pool = ThreadPool()
+    pool.map(process, tqdm(stock_list))
+    pool.close()
+    pool.join()
+    
+#    all_stock = tqdm(stock_list)
+#    for stock in all_stock:
+#        symbol = stock.split('.')[0]
+#        df = ts.pro_bar(ts_code=stock, adj='qfq', start_date=start_date , end_date=end_date)
+#        df.sort_index(axis=0, ascending = False, inplace = True)
+#        df.reset_index(drop=True, inplace=True)
+#        df = df.drop(['pre_close', 'change', 'amount'], axis=1)
+#        df.to_csv('Data/'+symbol+'.csv')
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ label.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  2 00:53:33 2020
+
+@author: Cyril
+"""
+
+def label_set(file):
+    
